38afd596976e43cada9fa674d4cf9e68c98bb091
amokrushin <anton@mokr.org>
2017-06-11 23:49:39 +0300

    skip postinstall step for travis

--- postinstall.js
+++ postinstall.js
@@ -5,11 +5,12 @@ const path = require('path');
 
 const opts = cwd => ({ stdio: 'inherit', cwd: path.join(__dirname, cwd) });
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production' && !process.env.TRAVIS) {
     execSync('yarn', opts('services/image-transform'));
     execSync('yarn', opts('services/store'));
     execSync('yarn', opts('services/uploads'));
     execSync('yarn', opts('shared/duk-task-queue'));
+    execSync('yarn link', opts('shared/duk-task-queue'));
     execSync('yarn link duk-task-queue', opts('services/store'));
     execSync('yarn link duk-task-queue', opts('shared/duk-task-queue'));
 }
