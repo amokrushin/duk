24d33a37b70c61b3ae89168cce9ca228c71bd714
amokrushin <anton@mokr.org>
2017-06-11 15:24:21 +0500

    cleanup

--- services/store/lib/util/hideUrlPassword.js
+++
@@ -1,8 +0,0 @@
-function hideUrlPassword(url) {
-    if (typeof url !== 'string') {
-        throw new TypeError(`URL must be a string: ${url}`);
-    }
-    return url.replace(/:[^/](.+)?@/, ':*****@').replace(/(password=).+(&?)/, '$1*****$2');
-}
-
-module.exports = hideUrlPassword;
--- services/store/lib/util/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable global-require */
-
-module.exports = {
-    hideUrlPassword: require('./hideUrlPassword'),
-    promisifyCallback: require('./promisifyCallback'),
-};
