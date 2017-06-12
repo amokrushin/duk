3a2e087b01e7fa2e65c0e3ece82e439e2164986e
amokrushin <anton@mokr.org>
2017-06-12 18:43:10 +0300

    update build process

---
+++ docker-deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Docker deploy trigger"
+echo "Tag $TRAVIS_TAG"
+
+for var in "$@"
+do
+    curl -X POST "$var" \
+        -H "Content-Type: application/json" \
+        --data '{"source_type": "Tag", "source_name": "'$TRAVIS_TAG'"}'
+done
