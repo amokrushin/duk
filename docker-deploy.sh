a25d636f4de681f8c85b8d34ab5a396b22eb3b66
amokrushin <anton@mokr.org>
2017-06-13 01:01:30 +0300

    test deploy config

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -3,12 +3,9 @@
 echo "Docker deploy trigger"
 echo "Tag $TRAVIS_TAG"
 
-#for var in "$@"
-#do
-#$TRAVIS_TAG
-#    curl --data build=true -X POST "$var"
-#    curl --data build=true -X POST "$1"
-curl -X POST "$1" \
-    -H "Content-Type: application/json" \
-    --data '{"source_type": "Tag", "source_name": "'$TRAVIS_TAG'"}'
-#done
+for var in "$@"
+do
+    curl -X POST "$1" \
+        -H "Content-Type: application/json" \
+        --data '{"source_type": "Tag", "source_name": "'$TRAVIS_TAG'"}'
+done
